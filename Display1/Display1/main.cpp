97a8bf813b2db8c1bda8a54bf0875ea674022863
PashaRobot <pavel.bezdelev@mail.ru>
2021-09-29 21:11:36 +0700

    With clear display
    Добавлена очистка дисплея

--- Display1/Display1/main.cpp
+++ Display1/Display1/main.cpp
@@ -10,18 +10,25 @@
 
 uint8_t PavelBezdelev[] = "Pavel Bezdelev";
 uint8_t FE19_08B[] = "FE19-08B";
+uint8_t ClearDisplay[] = "Clear Display";
 
 HD44780 FirstDisplay;
 
 int main(void)
-{
-	FirstDisplay.Set_X_Y(2, 1);
-	FirstDisplay.Send_String(PavelBezdelev);
-	FirstDisplay.Set_X_Y(5, 2);
-	FirstDisplay.Send_String(FE19_08B);
-	
+{	
     while (1) 
     {
+		FirstDisplay.Set_X_Y(2, 1);
+		FirstDisplay.Send_String(PavelBezdelev);
+		FirstDisplay.Set_X_Y(5, 2);
+		FirstDisplay.Send_String(FE19_08B);
+		_delay_ms(5000);
+		FirstDisplay.Clear_Display();
+		FirstDisplay.Set_X_Y(2, 1);
+		FirstDisplay.Send_String(ClearDisplay);
+		_delay_ms(2000);
+		FirstDisplay.Clear_Display();
+		_delay_ms(2000);
     }
 }
 
