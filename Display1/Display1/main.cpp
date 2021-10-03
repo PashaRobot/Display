68d6a51621e82d117585939a85db88cf1c150ae5
PashaRobot <pavel.bezdelev@mail.ru>
2021-10-03 14:22:26 +0700

    Добавлен русский язык

--- Display1/Display1/main.cpp
+++ Display1/Display1/main.cpp
@@ -8,9 +8,13 @@
 #include <avr/io.h>
 #include "HD44780.h"
 
-uint8_t PavelBezdelev[] = "Pavel Bezdelev";
-uint8_t FE19_08B[] = "FE19-08B";
-uint8_t Clear[] = "Cleaning display";
+uint8_t ABV[] = "ÀÁÂÃÄÅ¨ÆÇÈÉÊËÌÍÎ";
+uint8_t PRS[] = "ÏĞÑÒÓÔÕÖ×ØÙÚÛÜİŞ";
+uint8_t YAabv[] = "ßàáâãäå¸æçèéêëìí";
+uint8_t OPR[] = "îïğñòóôõö÷øùúûüışÿ";
+uint8_t uya[] = "şÿ";
+uint8_t proverka[] = "ÏĞÎÂÅĞÊÀ";
+uint8_t yazyka[] = "ĞÓÑÑÊÎÃÎ ßÇÛÊÀ";
 
 HD44780 FirstDisplay;
 
@@ -18,17 +22,24 @@ int main(void)
 {
     while (1) 
     {
-		FirstDisplay.Set_X_Y(2, 1);
-		FirstDisplay.Send_String(PavelBezdelev);
-		FirstDisplay.Set_X_Y(5, 2);
-		FirstDisplay.Send_String(FE19_08B);
+		FirstDisplay.Set_X_Y(5, 1);
+		FirstDisplay.Send_String(proverka);
+		FirstDisplay.Set_X_Y(2, 2);
+		FirstDisplay.Send_String(yazyka);
+		_delay_ms(3000);
+		FirstDisplay.Clear_Display();
+		FirstDisplay.Send_String(ABV);
+		FirstDisplay.Set_X_Y(1, 2);
+		FirstDisplay.Send_String(PRS);
 		_delay_ms(5000);
 		FirstDisplay.Clear_Display();
-		FirstDisplay.Set_X_Y(1, 1);
-		FirstDisplay.Send_String(Clear);
+		FirstDisplay.Send_String(YAabv);
+		FirstDisplay.Set_X_Y(1, 2);
+		FirstDisplay.Send_String(OPR);
+		_delay_ms(5000);
+		FirstDisplay.Clear_Display();
+		FirstDisplay.Send_String(uya);
 		_delay_ms(2000);
 		FirstDisplay.Clear_Display();
-		_delay_ms(2000);
     }
 }
-
