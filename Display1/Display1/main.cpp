0110553c4568e3064a79d1084888c255c8a9b445
PashaRobot <pavel.bezdelev@mail.ru>
2021-10-10 15:01:06 +0700

    Проверка работы спец. символов

--- Display1/Display1/main.cpp
+++ Display1/Display1/main.cpp
@@ -10,9 +10,40 @@
 
 HD44780 FirstDisplay; 
 
+uint8_t ArraySymbol[] =
+{
+	0b00000000,
+	0b00000000,
+	0b00001010,
+	0b00000000,
+	0b00010001,
+	0b00001110,
+	0b00000000,
+	0b00000000,
+	0b00000000,
+};
+
+void Set_CGRAM(uint8_t addr)
+{
+	FirstDisplay.Send_Cmd(0x40 | (addr & 0x3F));
+}
+
+void Set_Symbol(uint8_t* symbol)
+{
+	Set_CGRAM(0);
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		FirstDisplay.Send_Data(*(symbol + i));
+	}
+	FirstDisplay.Send_Cmd(0x80);
+}
+
 int main(void)
 {
-	uint8_t a = 0;
+	Set_Symbol(ArraySymbol);
+	FirstDisplay.Send_Data(0);
+	
+	/*uint8_t a = 0;
 	uint16_t b = 0;
 	int8_t c = 0;
 	int32_t d = 0;
@@ -33,5 +64,5 @@ int main(void)
 		c++;
 		d++;
 		_delay_ms(100);
-	}
+	}*/
 }
